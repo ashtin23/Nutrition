07a31a91ac9475e3861074f2f9e7fc8d6b276a10
ashtinlatting <rlatting92@gwu.edu>
2019-09-12 08:58:30 -0400

    error fixed

--- nutrition-app/src/components/List/List.js
+++ nutrition-app/src/components/List/List.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./List.css";
+
+import axios from "axios";
+
+const foodUrl = "http://localhost:3131/api/macronutrients";
+
 
 class List extends Component {
  constructor() {
-   super(props);
+   super();
  } 
  
   componentDidMount() {
-    // console.log(this.state);
+    // console.log(this.props);
     axios
       .get(foodUrl)
       .then(res => {
         console.log(res.data[0].report.foods[0].name)
         res.data[0].report.foods.forEach(i => {
-          let temp = this.state.name
+          let temp = this.props.name
           temp.push(i.name)
           this.setState({
             name: temp
