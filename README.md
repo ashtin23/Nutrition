23a77de857b7220bf8197068d4703934b904a572
ashtinlatting <rlatting92@gwu.edu>
2019-09-15 20:40:41 -0400

    still attempting to work out state issues

--- README.md
+++ README.md
@@ -48,18 +48,6 @@ until finished
 End with an example of getting some data out of the system or using it for a little demo
 
 
-### And coding style tests
-
-Explain what these tests test and why
-
-```
-Give an example
-```
-
-## Deployment
-
-Add additional notes about how to deploy this on a live system
-
 ## Built With
 
 * [MongoDB](http://www.dropwizard.io/1.0.2/docs/) - The database used
@@ -68,16 +56,11 @@ Add additional notes about how to deploy this on a live system
 * [Node.js](http://www.dropwizard.io/1.0.2/docs/) - The run-time environment used
 
 ## Contributing
-
-Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
-
-## Versioning
-
-We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+ct, and the process for submitting pull requests to us.
 
 ## Authors
 
-* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+* **Ashtin Latting** - *Initial work* - [](https://github.com/PurpleBooth)
 
 See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
 
--- nutrition-app/src/components/App/App.js
+++ nutrition-app/src/components/App/App.js
@@ -8,7 +8,8 @@ import Food from "../Food/Food";
 import "./App.css";
 import axios from "axios";
 
-const foodUrl = "http://localhost:3131/api/macronutrients";
+// const macroUrl = "http://localhost:3131/api/macronutrients";
+const foodUrl = "http://localhost:3131/api/food";
 
 class App extends Component {
   constructor(props) {
@@ -29,35 +30,46 @@ class App extends Component {
   }
 
 
-  componentDidMount() {
-    // console.log(this.state);
-    axios
-      .get(foodUrl)
+  componentDidMount(){
+    axios.get(foodUrl)
       .then(res => {
-        // let myData = res.data[0].report.foods[0];
-        res.data[0].report.foods.forEach(i => {
-          let temp = res.data[0].report.foods[i].name
-          temp.push(i.name)
-          this.setState({
-            name: res.data[0].report.foods[i].name,
-            servingSize: res.data[0].report.foods[i].measure,
-            calories: res.data[0].report.foods[i].nutrients[0],
-            macronutrients: [
-              {
-              fat: res.data[0].report.foods[i].nutrients[1],
-              protein: res.data[0].report.foods[i].nutrients[2],
-              carbohydrates: res.data[0].report.foods[i].nutrients[3]
-              }
-            ]
-          })
-       })
-       console.log(this.state)
-
+        console.log(res.data)
+        res.data[0].list.item.forEach(i => {
+          // let temp = res.data[0].list.item[i].name
+          this.setState({ name: res.data[0].list.item[i]})
+        })
       })
-      .catch(err => {
-        console.error(err);
-      });
   }
+
+  // componentDidMount() {
+  //   // console.log(this.state);
+  //   axios
+  //     .get(macroUrl)
+  //     .then(res => {
+  //       // let myData = res.data[0].report.foods[0];
+  //       res.data[0].report.foods.forEach(i => {
+  //         let temp = res.data[0].report.foods[i].name
+  //         temp.push(i.name)
+  //         this.setState({
+  //           name: res.data[0].report.foods[i].name,
+  //           servingSize: res.data[0].report.foods[i].measure,
+  //           calories: res.data[0].report.foods[i].nutrients[0],
+  //           macronutrients: [
+  //             {
+  //             fat: res.data[0].report.foods[i].nutrients[1],
+  //             protein: res.data[0].report.foods[i].nutrients[2],
+  //             carbohydrates: res.data[0].report.foods[i].nutrients[3]
+  //             }
+  //           ]
+  //         })
+  //      })
+  //      console.log(this.state)
+
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
+  // }
 
 //   handleClick = (e) => {
 //     console.log(e.target.key)
